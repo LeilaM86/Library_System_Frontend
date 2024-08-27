43f2e5ea1c2b2ad4e07889b292940fc1a6a56e10
Leila <leilamola86@gmail.com>
2024-08-27 21:03:59 +0200

    git init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { Outlet } from "react-router-dom";
+// import NavBar from "./components/NavBar";
+
+function App(): React.ReactElement {
+  return (
+    <>
+      {/* <NavBar />;
+      <Outlet /> */}
+    </>
+  );
+}
+export default App;
